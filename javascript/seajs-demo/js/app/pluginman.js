d6477a4e13b08727bc92c6d8eb289a29ba1bec4c
Hontyu Bhe <mfthits@gmail.com>
2013-06-02 12:15:28 +0800

    [seajs demo] load main when document ready

--- javascript/seajs-demo/js/app/pluginman.js
+++ javascript/seajs-demo/js/app/pluginman.js
@@ -2,6 +2,8 @@ define(function(require, exports, module){
 
     var plugins = seajs.config['data']['splugins']
     exports.pluginList = plugins
+
+    //get enabled plugins from config
     exports.getList =  function () {
         return plugins
     }
--- javascript/seajs-demo/js/main.js
+++ javascript/seajs-demo/js/main.js
@@ -1,18 +1,20 @@
-define(function(require) {
-    var _A_ = require("$")
-    seajs.log("::test jquery, document title is " + _A_(document).attr("title"))
+define(function(require, exports) {
+    exports.init = function () {
+        var _A_ = require("$")
+        seajs.log("::test jquery, document title is " + _A_(document).attr("title"))
 
-    var _U_ = require("_")
-    seajs.log("::test underscore: " + _U_.template("|<%-v%>|", {v:1}))
-    
+        var _U_ = require("_")
+        seajs.log("::test underscore: " + _U_.template("|<%-v%>|", {v:1}))
+        
 
-    var _B_ = require("backbone")
-    seajs.log("::test backbone, version is " + _B_.VERSION)
+        var _B_ = require("backbone")
+        seajs.log("::test backbone, version is " + _B_.VERSION)
 
-    var _C = require("./app/controller")
+        var _C = require("./app/controller")
 
-    seajs.log("::test simple mvc")
-    var c = new _C("demo_controller")
-    c.init()
-    c.render()
+        seajs.log("::test simple mvc")
+        var c = new _C("demo_controller")
+        c.init()
+        c.render()
+    }
 })
